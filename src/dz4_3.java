4059f628c6ef263ad62b436f9113f4c6b91df487
Irinka <irenmast@gmail.com>
2017-07-18 17:44:00 +0300

    6

--- src/dz4_3.java
+++ src/dz4_3.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
+/**
+ Перегрузить функцию drawRectangle (задание 2) таким образом, что бы она могла принимать на вход только 1 параметр (ширина квадрата)
+ и рисовать квадрат с равными сторонами Например 2 Вывод программы: + + + + Например 3 Вывод программы: + + + + + + + + +
+ */
 public class dz4_3 {
+    public static void drawRectangle(int x) {
+
+        if (x > 0) {
+            System.out.print("+");
+            drawRectangle(x - 1);
+        }
+
+    }
+
+    public static void drawRectangle(double x) {
+        System.out.print("+");
+        for (int i = 2; i < x; i++) {
+            System.out.print(" ");
+        }
+        System.out.println("+");
+
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Введите длину стороны квадрата");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+
+        drawRectangle(x);
+        System.out.println();
+
+
+        if(x>2){
+            for(int i=2; i < x; i++){
+                drawRectangle(x);
+
+                System.out.println();
+            }   }
+        drawRectangle(x);
+    }
 }
