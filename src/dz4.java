d19b9e213e376bb8785b351c0e3b31f9c7156a67
Ирина <irenmast@gmail.com>
2017-10-08 16:50:55 +0300

    Update dz4.java

--- src/dz4.java
+++ src/dz4.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+/*Write a function that counts in the console before the X number.
+Arguments of the function: the number X
 
+For example, X = 5.
+Output of the program:
+1
+2
+3
+4
+5*/
+
+/**
+ * Created by i on 11.07.2017.
+ */
 public class dz4 {
+    public static void printCifra(int x) {
+
+
+        if (x > 0) {
+            System.out.print(x+"; ");
+            printCifra(x - 1);
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        printCifra(x);
+}
 }
