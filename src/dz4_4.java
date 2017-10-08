b587928ee08e1fd6b0cfc662b5d14bfa1a4a17f9
Ирина <irenmast@gmail.com>
2017-10-08 16:54:06 +0300

    Update dz4_4.java

--- src/dz4_4.java
+++ src/dz4_4.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 /**
- Написать функцию getMax которая принимает на вход два аргумента в виде чисел. А возврыщает максимальное из этих двух.
- Так же, необходимо перегрузить эту функцию для работы с разными числовыми типами переменных (int, float)
+Write the getMax function, which takes two arguments as numbers. And returns the maximum of the two.
+ Also, you need to overload this function to work with different numeric variable types (int, float)
  */
 public class dz4_4 {
 
@@ -28,19 +28,19 @@ public class dz4_4 {
     }
 
     public static void main(String[] args) {
-        System.out.println("Введите первое число int");
+        System.out.println("Enter the first number int");
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
 
-        System.out.println("Введите второе число int");
+        System.out.println("Enter the second number int");
         Scanner sc1 = new Scanner(System.in);
         int y = sc1.nextInt();
 
-        System.out.println("Введите первое число float");
+        System.out.println("Enter the first number float");
         Scanner sc2 = new Scanner(System.in);
         float a = sc2.nextFloat();
 
-        System.out.println("Введите второе число float");
+        System.out.println("Enter the second number float");
         Scanner sc3 = new Scanner(System.in);
         float b = sc3.nextFloat();
 
@@ -48,8 +48,8 @@ public class dz4_4 {
         float maxFloat = getMax(a,b);
 
 
-        System.out.println("Максимальное число = " + maxInt);
-        System.out.println("Максимальное число float= " + maxFloat);
+        System.out.println("Maximum number int = " + maxInt);
+        System.out.println("Maximum number float= " + maxFloat);
 
     }
 
